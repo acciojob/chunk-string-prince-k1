339f58cc0bc20302cbb65cff5928bd550dcaff36
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-14 11:01:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function stringChop(str, size) {
   // your code here
+	let arr = str.split('');
+   let result = [];
+   for(let i = 0; i < arr.length; i++){
+      let sub = '';
+      let k = i;
+      for(let j = k; j < k + size; j++){
+         if(arr[j] != undefined){
+         sub += arr[j];
+      }
+      i = j;
+      }
+      result.push(sub);
+   }
+   return result;
 }
 
 // Do not change the code below
